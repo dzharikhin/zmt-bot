5b593bbd06617d7be9e5d69674319ba00facf8cf
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2025-01-20 17:33:58 +0300

    improved audio features calculation

--- audio/features.py
+++ audio/features.py
@@ -1,6 +1,8 @@
+import math
 import pathlib
-from math import ceil
-from typing import TypeAlias, cast
+import types
+from collections import OrderedDict
+from typing import TypeAlias, Callable, cast, Iterable
 
 import librosa
 import numpy as np
@@ -10,147 +12,82 @@ from scipy.sparse import csr_matrix
 from sklearn.cluster import AgglomerativeClustering
 from soundfile import SoundFile
 
-AudioFeaturesType: TypeAlias = tuple[
-    str,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-    float,
-]
+FRAMES_NUMBER = 160
+MFCCS_NUMBER = 48
+CHROMA_NUMBER = 12
+SPECTRAL_CONTRAST_NUMBER = 7
+TONNETZ_NUMBER = 6
+AGGREGATES = OrderedDict(
+    std=lambda col: col.std(),
+    skew=lambda col: col.skew(),
+    kurtosis=lambda col: col.kurtosis(),
+)
 
-AUDIO_FEATURE_SCHEMA = [
+
+def feature_size(**feature_columns: int) -> int:
+    feature_columns = list(feature_columns.values())[0]
+    return FRAMES_NUMBER * feature_columns + feature_columns * len(AGGREGATES)
+
+
+def build_schema_for_feature(
+    feature_prefix: str, feature_values: int
+) -> Iterable[tuple[str, type]]:
+    return (
+        *(
+            (f"{feature_prefix}_{frame + 1}_{column + 1}", float)
+            for frame in range(FRAMES_NUMBER)
+            for column in range(feature_values)
+        ),
+        *(
+            (f"{feature_prefix}_{aggregation}_{column + 1}", float)
+            for aggregation in AGGREGATES
+            for column in range(feature_values)
+        ),
+    )
+
+
+AUDIO_FEATURE_TYPE_SCHEMA = [
     ("track_id", str),
-    *((f"mfcc_mean{i + 1}", float) for i in range(64)),
-    *((f"chroma_mean{i + 1}", float) for i in range(12)),
-    *((f"spectral_contrast_mean{i + 1}", float) for i in range(7)),
-    ("zcr_mean", float),
-    ("rmse_mean", float),
-    ("spectral_centroid_mean", float),
-    ("spectral_bandwidth_mean", float),
-    ("spectral_flatness_mean", float),
-    *((f"tonnetz_mean{i + 1}", float) for i in range(6)),
+    *build_schema_for_feature("mfcc", MFCCS_NUMBER),
+    *build_schema_for_feature("chroma_cqt", CHROMA_NUMBER),
+    *build_schema_for_feature("chroma_cens", CHROMA_NUMBER),
+    *build_schema_for_feature("chroma_stft", CHROMA_NUMBER),
+    *build_schema_for_feature("zcr", 1),
+    *build_schema_for_feature("rmse", 1),
+    *build_schema_for_feature("spectral_centroid", 1),
+    *build_schema_for_feature("spectral_bandwidth", 1),
+    *build_schema_for_feature("spectral_flatness", 1),
+    *build_schema_for_feature("spectral_contrast", SPECTRAL_CONTRAST_NUMBER),
+    *build_schema_for_feature("spectral_rolloff", 1),
+    *build_schema_for_feature("tonnetz", TONNETZ_NUMBER),
     ("tempo", float),
 ]
 
-# features[name, 'std'] = np.std(values, axis=1)
-# features[name, 'skew'] = stats.skew(values, axis=1)
-# features[name, 'kurtosis'] = stats.kurtosis(values, axis=1)
-# features[name, 'median'] = np.median(values, axis=1)
-# features[name, 'min'] = np.min(values, axis=1)
-# features[name, 'max'] = np.max(values, axis=1)
+FeatureDataType = types.GenericAlias(
+    tuple, (float,) * (len(AUDIO_FEATURE_TYPE_SCHEMA) - 1)  # track_id
+)
+AudioFeaturesType: TypeAlias = tuple[str, *FeatureDataType]
 
 
 def extract_features_for_mp3(
     *,
     track_id: str,
     mp3_path: pathlib.Path,
-    frames_number: int,
 ) -> AudioFeaturesType:
     with SoundFile(mp3_path) as wav:
         audio, sr = librosa.load(wav, sr=None)
-        frames = librosa.stft(audio).shape[1]
+        stft = librosa.stft(audio)
+        frames = stft.shape[1]
 
         connectivity_matrix = csr_matrix((frames, frames), dtype=np.int8)
         connectivity_matrix.setdiag(values=[1] * frames, k=-1)
         connectivity_matrix.setdiag(values=[1] * frames, k=1)
 
-        def clusterize_framed_feature(
+        def build_optimal_frame_clusterization(
             feature_data: np.ndarray,
-            feature_prefix: str,
-            guiding_values_fraction: float = 0.37,
+            guiding_values_fraction: float = 0.1,
         ) -> pl.DataFrame:
-            feature_column_builder = lambda i: f"{feature_prefix}_{i}"
+            feature_column_builder = lambda i: f"metric_{i}"
             feature_raw = pl.DataFrame(
                 data=feature_data,
                 schema=[
@@ -163,7 +100,7 @@ def extract_features_for_mp3(
                 .transpose(column_names=["std"])
                 .with_row_index()
                 .sort(by=pl.col("std"), descending=True)
-                .head(ceil(feature_raw.shape[1] * guiding_values_fraction))
+                .head(math.ceil(feature_raw.shape[1] * guiding_values_fraction))
                 .get_column("index")
                 .sort()
                 .to_list()
@@ -172,7 +109,7 @@ def extract_features_for_mp3(
             def cluster_values(raw_values: pl.Series) -> pl.Series:
                 raw_values = raw_values.to_numpy().reshape(-1, 1)
                 agglomerator = AgglomerativeClustering(
-                    n_clusters=frames_number, connectivity=connectivity_matrix
+                    n_clusters=FRAMES_NUMBER, connectivity=connectivity_matrix
                 )
                 cluster_list = agglomerator.fit_predict(raw_values)
                 return pl.Series(cluster_list)
@@ -220,57 +157,111 @@ def extract_features_for_mp3(
                     reverse=True,
                 )
             )[0][0]
-            feature_aggregate = clusters[target_cluster_variant_index]
-            return feature_aggregate.unstack(1)
+            return clusterization_variants.select(
+                pl.col(f"clusterization_{target_cluster_variant_index}")
+            )
 
-        mfcc = clusterize_framed_feature(
-            feature.mfcc(y=audio, sr=sr, n_mfcc=64),
-            "mfcc",
+        def wrap_in_df(feature_data: np.ndarray, feature_prefix: str) -> pl.DataFrame:
+            feature_column_builder = lambda i: f"{feature_prefix}_{i}"
+            df = pl.DataFrame(
+                data=(
+                    feature_data
+                    if feature_data.shape[0] > 1
+                    else feature_data.reshape((-1, 1))
+                ),
+                schema=[
+                    feature_column_builder(i) for i in range(feature_data.shape[0])
+                ],
+                orient="col" if feature_data.shape[0] > 1 else "row",
+            )
+            if df.shape[0] != feature_data.shape[1]:
+                raise Exception(
+                    f"shape of feature {feature_prefix} dataframe: {df.shape} does not match feature data shape {feature_data.shape}"
+                )
+            return df
+
+        def clustered_frames_mean(
+            feature_df: pl.DataFrame,
+            clusterization: pl.DataFrame,
+        ) -> pl.DataFrame:
+            return (
+                pl.concat(
+                    [
+                        feature_df,
+                        clusterization,
+                    ],
+                    how="horizontal",
+                )
+                .group_by(by=pl.col(clusterization.columns[0]))
+                .agg(pl.all().mean())
+                .drop("by", clusterization.columns[0])
+                .unstack(1)
+            )
+
+        def aggregate_feature(
+            feature_df: pl.DataFrame,
+            aggregation_postfix: str,
+            column_aggregation: Callable[[pl.Expr], pl.Expr],
+        ) -> pl.DataFrame:
+            return feature_df.rename(
+                {
+                    old_column: f"{old_column}_{aggregation_postfix}"
+                    for old_column in feature_df.columns
+                }
+            ).select(
+                *(column_aggregation(pl.nth(i)) for i in range(feature_df.shape[1]))
+            )
+
+        features_data = OrderedDict(
+            mfcc=librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=MFCCS_NUMBER),
+            chroma_cqt=librosa.feature.chroma_cqt(
+                y=audio, sr=sr, n_chroma=CHROMA_NUMBER
+            ),
+            chroma_cens=librosa.feature.chroma_cens(
+                y=audio, sr=sr, n_chroma=CHROMA_NUMBER
+            ),
+            chroma_stft=librosa.feature.chroma_stft(
+                y=audio, sr=sr, n_chroma=CHROMA_NUMBER
+            ),
+            zcr=librosa.feature.zero_crossing_rate(y=audio),
+            rmse=librosa.feature.rms(y=audio),
+            spectral_centroid=librosa.feature.spectral_centroid(y=audio, sr=sr),
+            spectral_bandwidth=librosa.feature.spectral_bandwidth(y=audio, sr=sr),
+            spectral_flatness=librosa.feature.spectral_flatness(y=audio),
+            spectral_contrast=librosa.feature.spectral_contrast(
+                y=audio, sr=sr, n_bands=SPECTRAL_CONTRAST_NUMBER - 1
+            ),
+            spectral_rolloff=librosa.feature.spectral_rolloff(y=audio, sr=sr),
+            tonnetz=librosa.feature.tonnetz(y=audio, sr=sr),  # size always eq(6)
         )
+        all_features = np.concatenate(list(features_data.values()))
+        optimal_frame_clusterization = build_optimal_frame_clusterization(all_features)
 
-        chroma = feature.chroma_stft(y=audio, sr=sr)
-        chroma_mean = np.mean(chroma.T, axis=0)
+        def compress_feature(
+            feature_data: np.ndarray, feature_prefix: str
+        ) -> pl.DataFrame:
+            feature_df = wrap_in_df(feature_data, feature_prefix)
+            clustered_frames_mean_feature = clustered_frames_mean(
+                feature_df, optimal_frame_clusterization
+            )
+            return pl.concat(
+                [clustered_frames_mean_feature]
+                + [
+                    aggregate_feature(feature_df, aggregation_name, aggregation_action)
+                    for aggregation_name, aggregation_action in AGGREGATES.items()
+                ],
+                how="horizontal",
+            )
 
-        spectral_contrast = feature.spectral_contrast(y=audio, sr=sr)
-        spectral_contrast_mean = np.mean(spectral_contrast.T, axis=0)
+        compressed_data = OrderedDict(
+            (k, compress_feature(v, k)) for k, v in features_data.items()
+        )
+        tempo = pl.Series("tempo", librosa.feature.tempo(y=audio, sr=sr)).to_frame()
 
-        zcr = feature.zero_crossing_rate(y=audio, frame_length=2048, hop_length=512)
-        zcr_mean = np.mean(zcr.T, axis=0)
-
-        rmse = librosa.feature.rms(y=audio)
-        rmse_mean = np.mean(rmse.T, axis=0)
-
-        spectral_centroid = librosa.feature.spectral_centroid(y=audio, sr=sr)
-        spectral_centroid_mean = np.mean(spectral_centroid.T, axis=0)
-
-        spectral_bandwidth = librosa.feature.spectral_bandwidth(y=audio, sr=sr)
-        spectral_bandwidth_mean = np.mean(spectral_bandwidth.T, axis=0)
-
-        spectral_flatness = librosa.feature.spectral_flatness(y=audio)
-        spectral_flatness_mean = np.mean(spectral_flatness.T, axis=0)
-
-        tonnetz = librosa.feature.tonnetz(y=audio, sr=sr)
-        tonnetz_mean = np.mean(tonnetz.T, axis=0)
-
-        onset_env = librosa.onset.onset_strength(y=audio, sr=sr)
-        tempo = feature.tempo(y=audio, sr=sr, onset_envelope=onset_env)
-
-        # feature_row = np.concatenate(
-        #     (
-        #         mfccs_mean,
-        #         chroma_mean,
-        #         spectral_contrast_mean,
-        #         zcr_mean,
-        #         rmse_mean,
-        #         spectral_centroid_mean,
-        #         spectral_bandwidth_mean,
-        #         spectral_flatness_mean,
-        #         tonnetz_mean,
-        #         tempo,
-        #     )
-        # )
-
-        return cast(AudioFeaturesType, (track_id, *tuple(feature_row)))
+    feature_row = pl.concat(
+        [v for v in compressed_data.values()] + [tempo], how="horizontal"
+    )
+    return cast(AudioFeaturesType, (track_id, *tuple(feature_row.rows()[0])))
 
 
 if __name__ == "__main__":
@@ -279,6 +270,6 @@ if __name__ == "__main__":
     row = extract_features_for_mp3(
         track_id=track.stem,
         mp3_path=track,
-        frames_number=160,
     )
-    print(row)
+    print(f"schema size: {len(AUDIO_FEATURE_TYPE_SCHEMA)}")
+    print(f"row size: {len(row)}")
