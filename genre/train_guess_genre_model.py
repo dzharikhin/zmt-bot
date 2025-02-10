9d22c13b413bc22e596924a59eccca6a8be54aa2
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2025-02-10 18:51:40 +0300

    use genre interest way

--- genre/train_guess_genre_model.py
+++ genre/train_guess_genre_model.py
@@ -3,16 +3,23 @@ import logging
 import pathlib
 import pickle
 import random
+from functools import reduce
 
+import numpy
 import numpy as np
 import polars as pl
 from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 from sklearn.inspection import permutation_importance
 from sklearn.kernel_approximation import Nystroem
 from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import cross_val_score
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
+
+from genre.create_genre_mapping import group_genres
+from genre.filter_outliers import filter_outliers_per_genre
+from genre.prepare_features_dataset import prepare_audio_features_dataset
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,55 +41,147 @@ def main(
     genre_sample_fraction: float = 1.0,
     feature_importance_repetitions: int = 0,
 ):
-    # audio_features_dataset_path = prepare_audio_features_dataset(
-    #     working_dir, snippets_path
-    # )
-    # mapped_genres_dataset_path = working_dir.joinpath("songs-mapped_genres.csv")
-    # group_genres(
-    #     genre_dataset_path=songs_dataset_path,
-    #     grouped_by_genre_path=working_dir.joinpath("songs-grouped_by_genre.csv"),
-    #     mapped_genre_dataset_path=mapped_genres_dataset_path,
-    # )
+    audio_features_dataset_path = prepare_audio_features_dataset(
+        working_dir, snippets_path
+    )
+    mapped_genres_dataset_path = working_dir.joinpath("songs-mapped_genres.csv")
+    group_genres(
+        genre_dataset_path=songs_dataset_path,
+        grouped_by_genre_path=working_dir.joinpath("songs-grouped_by_genre.csv"),
+        mapped_genre_dataset_path=mapped_genres_dataset_path,
+    )
     filtered_genre_dataset_path = working_dir.joinpath("songs-genre_filtered.csv")
-    # filter_outliers_per_genre(
-    #     genre_dataset_path=mapped_genres_dataset_path,
-    #     audio_features_dataset_path=audio_features_dataset_path,
-    #     result_path=filtered_genre_dataset_path,
-    #     contamination_fraction=data_contamination_fraction,
-    # )
+    filter_outliers_per_genre(
+        genre_dataset_path=mapped_genres_dataset_path,
+        audio_features_dataset_path=audio_features_dataset_path,
+        result_path=filtered_genre_dataset_path,
+        contamination_fraction=data_contamination_fraction,
+    )
 
-    audio_features_dataset_path = working_dir.joinpath("audio_features_dataset.csv")
     genre_dataset = pl.scan_csv(filtered_genre_dataset_path)
 
-    meaningful_genres_data = (
-        genre_dataset.group_by(pl.col(GENRE_COLUMN_NAME))
-        .agg(pl.len().ge(100).alias("examples_cnt"))
-        .filter(
-            pl.col("examples_cnt")
-            .and_(pl.col(GENRE_COLUMN_NAME).str.ends_with("folk").not_())
-            .and_(pl.col(GENRE_COLUMN_NAME).str.ends_with("instrumental").not_())
+    genre_mapping = {
+        "edm": {
+            "trance",
+            "minimal techno",
+            "psytrance",
+            "electro",
+            "techno",
+            "house",
+            "progressive",
+            "miami bass",
+            "electro tropical",
+            "baile funk",
+        },
+        "hardstyle": {},
+        "breaks": {"breakbeat"},
+        "hip hop": {
+            "electronic trap",
+            "trap",
+            "r&b",
+        },
+        "trip hop": {},
+        "drum and bass": {
+            "breakcore",
+            "neurofunk",
+            "jungle",
+            "jump up",
+            "liquid",
+        },
+        "dubstep": {
+            "minimal dubstep",
+            "substep",
+        },
+        "rock": {
+            "grunge",
+            "metal rock",
+            "ska",
+            "death metal",
+            "post rock",
+            "emo rock",
+            "punk rock",
+            "nu metal",
+            "black metal",
+            "metalcore",
+        },
+        "pop": {},
+        "reggae": {
+            "ragga",
+        },
+        "jazz": {},
+        "funk": {},
+        "blues": {},
+        "witch house": {},
+    }
+    interesting_genres = set(genre_mapping.keys())
+    interesting_genres.update(reduce(lambda a, b: a.union(b), genre_mapping.values()))
+    not_included_genres = list(
+        sorted(
+            filter(
+                lambda g: not g.endswith("folk") and not g.endswith("instrumental"),
+                set(
+                    genre_dataset.unique(subset=[GENRE_COLUMN_NAME])
+                    .sort(by=GENRE_COLUMN_NAME)
+                    .collect()
+                    .get_column(GENRE_COLUMN_NAME)
+                    .to_list()
+                )
+                - interesting_genres,
+            )
         )
-        .select(pl.col(GENRE_COLUMN_NAME))
-        .collect()
     )
-    genre_sample = meaningful_genres_data
+    logging.info(f"{not_included_genres=}")
+    inverse_mapping = {
+        specific_name: common_name
+        for common_name, specific_names in genre_mapping.items()
+        for specific_name in specific_names
+    }
+
+    def mapper(genre_name: str) -> str:
+        return inverse_mapping.get(genre_name, genre_name)
+
+    genre_sample = (
+        genre_dataset.with_columns(
+            pl.col(GENRE_COLUMN_NAME)
+            .map_elements(mapper, pl.String)
+            .alias("clustered_genre")
+        )
+        .with_columns(pl.col("clustered_genre").alias(GENRE_COLUMN_NAME))
+        .select(pl.col(ID_COLUMN_NAME), pl.col(GENRE_COLUMN_NAME))
+        .filter(pl.col(GENRE_COLUMN_NAME).is_in(list(genre_mapping.keys())))
+    )
+
     if genre_sample_fraction < 1.0:
         genre_sample = genre_sample.sample(fraction=genre_sample_fraction, shuffle=True)
-    logging.info(f"{genre_sample.shape=}: {genre_sample.sort(by=pl.col(GENRE_COLUMN_NAME)).to_series().to_list()=}")
+    logging.info(
+        f"{genre_sample.shape=}: {genre_sample.sort(by=pl.col(GENRE_COLUMN_NAME)).to_series().to_list()=}"
+    )
     genre_dataset_sample = genre_dataset.select(
         pl.col(ID_COLUMN_NAME), pl.col(GENRE_COLUMN_NAME)
-    ).filter(pl.col(GENRE_COLUMN_NAME).is_in(genre_sample.get_column(GENRE_COLUMN_NAME)))
-
-    test_track_ids = genre_dataset_sample.group_by(pl.col(GENRE_COLUMN_NAME)).agg(
-        pl.col(ID_COLUMN_NAME).sample(fraction=0.3, with_replacement=False, shuffle=True)
-    ).explode(pl.col(ID_COLUMN_NAME)).select(ID_COLUMN_NAME).collect()
-
-    normalized_audio_features_dataset = (pl.scan_csv(audio_features_dataset_path)
-    .drop_nans()
-    .select(
-        pl.col(ID_COLUMN_NAME),
-        (pl.all().exclude(ID_COLUMN_NAME) - pl.all().exclude(ID_COLUMN_NAME).mean()) / pl.all().exclude(ID_COLUMN_NAME).std()
+    ).filter(
+        pl.col(GENRE_COLUMN_NAME).is_in(genre_sample.get_column(GENRE_COLUMN_NAME))
     )
+
+    test_track_ids = (
+        genre_dataset_sample.group_by(pl.col(GENRE_COLUMN_NAME))
+        .agg(
+            pl.col(ID_COLUMN_NAME).sample(
+                fraction=0.3, with_replacement=False, shuffle=True
+            )
+        )
+        .explode(pl.col(ID_COLUMN_NAME))
+        .select(ID_COLUMN_NAME)
+        .collect()
+    )
+
+    normalized_audio_features_dataset = (
+        pl.scan_csv(audio_features_dataset_path)
+        .drop_nans()
+        .select(
+            pl.col(ID_COLUMN_NAME),
+            (pl.all().exclude(ID_COLUMN_NAME) - pl.all().exclude(ID_COLUMN_NAME).mean())
+            / pl.all().exclude(ID_COLUMN_NAME).std(),
+        )
     )
     data = normalized_audio_features_dataset.join(
         genre_dataset_sample.select(pl.col(ID_COLUMN_NAME), pl.col(GENRE_COLUMN_NAME)),
@@ -92,16 +191,25 @@ def main(
     )
     logging.info(f"Collecting data")
     data = data.collect()
-    compressed_data = data
     logging.info(f"Compressing data")
-    feature_map_nystroem = Nystroem(random_state=42, n_components=5000).set_output(transform="polars")
-    compressed_data = (feature_map_nystroem.fit_transform(data.select(pl.all().exclude(ID_COLUMN_NAME, GENRE_COLUMN_NAME)))
-                       .insert_column(1, data.get_column(GENRE_COLUMN_NAME))
-                       .insert_column(1, data.get_column(ID_COLUMN_NAME)))
+    feature_map_nystroem = Nystroem(
+        random_state=42, n_components=data.shape[1] // 2
+    ).set_output(transform="polars")
+    compressed_data = (
+        feature_map_nystroem.fit_transform(
+            data.select(pl.all().exclude(ID_COLUMN_NAME, GENRE_COLUMN_NAME))
+        )
+        .insert_column(1, data.get_column(GENRE_COLUMN_NAME))
+        .insert_column(1, data.get_column(ID_COLUMN_NAME))
+    )
     del data
     logging.info(f"Splitting train-test")
-    test_data = compressed_data.filter(pl.col(ID_COLUMN_NAME).is_in(test_track_ids.get_column(ID_COLUMN_NAME)))
-    train_data = compressed_data.filter(pl.col(ID_COLUMN_NAME).is_in(test_track_ids.get_column(ID_COLUMN_NAME)).not_())
+    test_data = compressed_data.filter(
+        pl.col(ID_COLUMN_NAME).is_in(test_track_ids.get_column(ID_COLUMN_NAME))
+    )
+    train_data = compressed_data.filter(
+        pl.col(ID_COLUMN_NAME).is_in(test_track_ids.get_column(ID_COLUMN_NAME)).not_()
+    )
 
     random_seeds_for_models = {
         name: random.randint(1, 100) for name in ("ovo", "rf", "mlp")
@@ -123,47 +231,48 @@ def main(
             (
                 "rf",
                 RandomForestClassifier(
-                    n_estimators=train_data.shape[1] // 10,
+                    n_estimators=compressed_data.shape[1] // 10,
                     random_state=random_seeds_for_models["rf"],
                     n_jobs=parallel_threads,
                 ),
             ),
-            ("mlp", MLPClassifier(random_state=random_seeds_for_models["mlp"])),
+            (
+                "mlp",
+                MLPClassifier(
+                    hidden_layer_sizes=(compressed_data.shape[1] // 10,),
+                    random_state=random_seeds_for_models["mlp"],
+                ),
+            ),
         ],
     )
 
-    # cv_scores = cross_val_score(model, X_train, y_train, cv=5, n_jobs=parallel_threads)
-    # logging.info(f"Cross-Validation Scores: {cv_scores}")
-    # logging.info(f"Mean Cross-Validation Score: {numpy.mean(cv_scores):.2f}")
+    X_train = train_data.select(pl.all().exclude(ID_COLUMN_NAME, GENRE_COLUMN_NAME))
+    y_train = train_data.select(pl.col(GENRE_COLUMN_NAME))
+
+    cv_scores = cross_val_score(model, X_train, y_train, cv=5, n_jobs=parallel_threads)
+    logging.info(f"Cross-Validation Scores: {cv_scores}")
+    logging.info(f"Mean Cross-Validation Score: {numpy.mean(cv_scores):.2f}")
 
     logging.info("Fitting")
-    model.fit(
-        train_data.select(pl.all().exclude(ID_COLUMN_NAME, GENRE_COLUMN_NAME)),
-        train_data.select(pl.col(GENRE_COLUMN_NAME)),
-    )
-    # for train_chunk in train_data.iter_slices():
-    #     X = train_chunk.select(pl.all().exclude(ID_COLUMN_NAME, GENRE_COLUMN_NAME))
-    #     y = train_chunk.select(GENRE_COLUMN_NAME)
-    #     logging.info(f"{X=}")
-    #     logging.info(f"{np.argwhere(np.isnan(X))=}")
-    #     logging.info(f"{y=}")
-    #     model.partial_fit(X, y, genre_sample)
+    model.fit(X_train, y_train,)
 
     logging.info(f"Fitting is done")
     logging.info(f"Collecting test data")
     X_test = test_data.select(pl.all().exclude(ID_COLUMN_NAME, GENRE_COLUMN_NAME))
     logging.info(f"Collecting model predictions")
+
     def partial_predict(chunk):
         predicted_part = model.predict(chunk)
         logging.info(f"Predictions for {chunk.shape=}: {predicted_part.shape}")
         return predicted_part
-    y_predicted = np.concatenate([partial_predict(data_chunk) for data_chunk in X_test.iter_slices(1000)])
+
+    y_predicted = np.concatenate(
+        [partial_predict(data_chunk) for data_chunk in X_test.iter_slices(1000)]
+    )
     logging.info(f"Collecting test predictions")
     y_test = test_data.select(GENRE_COLUMN_NAME)
     logging.info(f"Evaluating predictions test")
-    logging.info(
-        f"Average accuracy: {accuracy_score(y_test, y_predicted):.2f}"
-    )
+    logging.info(f"Average accuracy: {accuracy_score(y_test, y_predicted):.2f}")
     report = classification_report(
         y_test,
         y_predicted,
@@ -173,7 +282,7 @@ def main(
 
     not_learned_genres = 0
     report_rows = [("genre", "precision", "examples in data")]
-    for genre, data in sorted(
+    for genre, stats in sorted(
         filter(
             lambda t: isinstance(t[1], dict)
             and t[0] not in ["macro avg", "weighted avg"],
@@ -182,8 +291,8 @@ def main(
         key=lambda t: t[1]["precision"],
         reverse=True,
     ):
-        report_rows.append((genre, data["precision"], int(data["support"])))
-        if data["precision"] <= 0.5:
+        report_rows.append((genre, stats["precision"], int(stats["support"])))
+        if stats["precision"] <= 0.5:
             not_learned_genres += 1
 
     logging.info(f"Dumping model")
