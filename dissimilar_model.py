41d5e9bd17ae4c913af5bd0640b46f9a0567a72d
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2025-05-28 13:53:14 +0300

    tune

---
+++ dissimilar_model.py
@@ -0,0 +1,9 @@
+import polars as pl
+
+from train import train_dissimilar_model
+
+if __name__ == "__main__":
+    train_data = pl.read_csv("data/train.csv")
+    test_data = pl.read_csv("data/test.csv")
+    model, accuracy = train_dissimilar_model(train_data=train_data, test_data=test_data, nu=0.05)
+    print(accuracy)
