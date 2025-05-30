b2f3b94da4d5af702fc8eb4b11e21cb6e9a351a4
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2025-05-30 17:45:10 +0300

    test if streaming works

--- dataset/persistent_dataset_processor.py
+++ dataset/persistent_dataset_processor.py
@@ -3,7 +3,6 @@ import pathlib
 import shutil
 import tempfile
 from datetime import datetime
-from functools import lru_cache
 from typing import (
     Any,
     Callable,
@@ -43,8 +42,6 @@ class DataSetFromDataManager(DatasetProcessor[ID]):
         index_generator: Generator[ID, None, None],
         intermediate_results_dir: pathlib.Path,
         batch_size: int = 1000,
-        map_elements_call_strategy: MapElementsStrategy = None,
-        cache_fraction: float = 0,
     ):
         self._persist_path = csv_path
         self.row_schema = row_schema
@@ -52,10 +49,8 @@ class DataSetFromDataManager(DatasetProcessor[ID]):
             (name, pl.DataType.from_python(column_type))
             for name, column_type in row_schema
         ]
-        self.cache_fraction = cache_fraction
-        self._map_elements_call_strategy = map_elements_call_strategy
-        self._intermediate_results_dir = intermediate_results_dir
         self._batch_size = batch_size
+        self._intermediate_results_dir = intermediate_results_dir
         self._df = self._init_df(index_generator)
         size_df = (
             self._df.select(pl.nth(0), pl.nth(1))
@@ -70,62 +65,51 @@ class DataSetFromDataManager(DatasetProcessor[ID]):
         self.to_process_rows_count = (
             size_df.filter(pl.col("processed").eq(False)).select("len").sum().item()
         )
-        self._backup_file: pathlib.Path | None = None
         print(f"Successfully inited {csv_path} manager. Ready to fill")
 
     def fill(self, row_value_generator: Callable[[ID], tuple[ID, *tuple[Any, ...]]]):
         mapper = functools.partial(self._transform_tuple_to_dict, row_value_generator)
-        if self.cache_fraction > 0:
-            mapper = lru_cache(
-                maxsize=int(self.to_process_rows_count * self.cache_fraction)
-            )(mapper)
-        map_elements_kwargs = (
-            {"strategy": self._map_elements_call_strategy}
-            if self._map_elements_call_strategy
-            else {}
-        )
         id_col_name = self._polars_row_schema[0][0]
         generated_data_col_name = "generated_row_data_as_struct"
 
-        def process_unprocessed_rows_in_batch(df: pl.DataFrame) -> pl.DataFrame:
-            additional_data = (
-                df.with_columns(
-                    pl.nth(0)
-                    .map_elements(
-                        mapper,
-                        pl.Struct(dict(self._polars_row_schema)),
-                        **map_elements_kwargs,
-                    )
-                    .alias(generated_data_col_name)
-                )
-                .with_columns(pl.col(generated_data_col_name).struct.unnest())
-                .drop(generated_data_col_name)
-            )
-            return df.update(additional_data, on=id_col_name, how="left")
+        # def process_unprocessed_rows_in_batch(df: pl.DataFrame) -> pl.DataFrame:
+        #     additional_data = (
+        #         df.with_columns(
+        #             pl.nth(0)
+        #             .map_elements(
+        #                 mapper,
+        #                 pl.Struct(dict(self._polars_row_schema)),
+        #             )
+        #             .alias(generated_data_col_name)
+        #         )
+        #         .with_columns(pl.col(generated_data_col_name).struct.unnest())
+        #         .drop(generated_data_col_name)
+        #     )
+        #     return df.update(additional_data, on=id_col_name, how="left")
 
-        not_processes_yet_df = self._df.filter(pl.nth(1).is_null()).map_batches(
-            process_unprocessed_rows_in_batch, streamable=True
-        )
         print(
-            f"Processing {self.to_process_rows_count} of total {self.total_dataset_rows_count} rows"
+            f"Processing {self.to_process_rows_count} of total {self.total_dataset_rows_count} rows with batch={self._batch_size}"
         )
-        batch_index = 0
-        while not (
-            slice_df := not_processes_yet_df.slice(
-                batch_index * self._batch_size, self._batch_size
-            ).collect(engine="streaming")
-        ).is_empty():
-            with self._persist_path.open("at") as sink:
-                slice_df.write_csv(
-                    sink,
-                    include_header=(self._persist_path.stat().st_size == 0),
+
+        # self._df.filter(pl.nth(1).is_null()).map_batches(
+        #     process_unprocessed_rows_in_batch, streamable=True
+        # ).sink_csv(self._intermediate_results_dir.joinpath("result.csv"), batch_size=self._batch_size, engine="streaming", sync_on_close="all")
+        (self._df
+         .filter(pl.nth(1).is_null())
+         .with_columns(
+            pl.nth(0)
+                .map_elements(
+                    mapper,
+                    pl.Struct(dict(self._polars_row_schema)),
                 )
-            print(
-                f"Batch [{batch_index * self._batch_size}, {(batch_index + 1) * self._batch_size}) of not processed yet rows is appended to {self._persist_path}"
-            )
-            batch_index += 1
+                .alias(generated_data_col_name)
+        )
+         .with_columns(pl.col(generated_data_col_name).struct.unnest())
+         .drop(generated_data_col_name)
+         .sink_csv(self._intermediate_results_dir.joinpath("result.csv"), batch_size=1, sync_on_close="all"))
+
         print(
-            f"Successfully processed all of {self.total_dataset_rows_count} rows, result in {self._persist_path}"
+            f"Successfully processed all of {self.total_dataset_rows_count} rows"
         )
 
     def __enter__(self) -> DatasetProcessor[ID]:
@@ -136,6 +120,7 @@ class DataSetFromDataManager(DatasetProcessor[ID]):
                 self._persist_path,
                 self._backup_file,
             )
+            self._tmp_result_file = self._intermediate_results_dir.joinpath("result.csv")
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -144,9 +129,15 @@ class DataSetFromDataManager(DatasetProcessor[ID]):
                 self._backup_file,
                 self._persist_path.parent.joinpath(self._backup_file.name),
             )
+        elif self._tmp_result_file and self._tmp_result_file.exists():
+            shutil.copy(
+                self._tmp_result_file,
+                self._persist_path,
+            )
+
 
     def remove_failures_in_place(self, failed_row_ids: Collection[ID]):
-        tmp_file = self._intermediate_results_dir.joinpath("cleared_data")
+        tmp_file = self._intermediate_results_dir.joinpath("filtered_data.csv")
         drop_ids = list(failed_row_ids)
         pl.scan_csv(self._persist_path, schema=dict(self._polars_row_schema)).filter(
             pl.nth(0).is_in(drop_ids).not_()
@@ -157,41 +148,23 @@ class DataSetFromDataManager(DatasetProcessor[ID]):
         schema_as_dict = dict(self._polars_row_schema)
         index_column_name = self._polars_row_schema[0][0]
 
-        if self._persist_path.exists() and self._persist_path.stat().st_size > 0:
-            existing_data_df = pl.scan_csv(self._persist_path, schema=schema_as_dict)
-        else:
-            existing_data_df = pl.LazyFrame([], schema=schema_as_dict)
-
-        print(
-            f"Using {self._intermediate_results_dir} as tempdir for data initialization"
-        )
-        index_buffer_file_path = pathlib.Path(self._intermediate_results_dir).joinpath(
-            "index_buffer"
-        )
-        with index_buffer_file_path.open(mode="wt") as index_buffer_file:
-            index_buffer_file.write(
-                f"{index_column_name},{",".join([name for name, _ in self.row_schema][1:])}\n"
-            )
-            for index_value in index_generator:
+        if not self._persist_path.exists() or self._persist_path.stat().st_size <= 0:
+            with self._persist_path.open(mode="wt") as index_buffer_file:
                 index_buffer_file.write(
-                    f"{index_value}{"," * (len(self.row_schema) - 1)}\n"
+                    f"{index_column_name},{",".join([name for name, _ in self.row_schema][1:])}\n"
                 )
-        whole_data_df = pl.scan_csv(
-            index_buffer_file_path, schema=schema_as_dict, has_header=True
-        )
-        whole_data_df = whole_data_df.update(
-            existing_data_df, on=index_column_name, how="left", include_nulls=False
-        )
-        data_file = pathlib.Path(self._intermediate_results_dir).joinpath("merged")
-        merged_data = whole_data_df.collect(engine="streaming")
-        merged_data.write_csv(data_file)
-        return pl.scan_csv(data_file, schema=schema_as_dict)
+                for index_value in index_generator:
+                    index_buffer_file.write(
+                        f"{index_value}{"," * (len(self.row_schema) - 1)}\n"
+                    )
+
+        return pl.scan_csv(self._persist_path, schema=schema_as_dict)
 
     def _transform_tuple_to_dict(
         self,
         row_generator: Callable[[ID], tuple[ID, *tuple[Any, ...]]],
         row_id: ID,
-    ) -> dict[str:ID, str:Any]:
+    ) -> dict[ID, Any]:
         result_tuple = row_generator(row_id)
         result = {}
         fails = {}
@@ -218,9 +191,12 @@ class DataSetFromDataManager(DatasetProcessor[ID]):
 
 
 if __name__ == "__main__":
+    pl.show_versions()
     path = pathlib.Path("snippet-dataset.csv")
     counter = atomics.atomic(width=4, atype=atomics.INT)
-    with tempfile.TemporaryDirectory() as tmp:
+    tmp_path = pathlib.Path("tmp")
+    tmp_path.mkdir(exist_ok=True)
+    with tempfile.TemporaryDirectory(dir=tmp_path, delete=False) as tmp:
         dataset_manager = DataSetFromDataManager(
             path,
             row_schema=(
@@ -234,13 +210,14 @@ if __name__ == "__main__":
             ),
             intermediate_results_dir=pathlib.Path(tmp),
             batch_size=1,
-            cache_fraction=0,
         )
         with dataset_manager as ds:
 
             def generate_value(row_id: str) -> tuple[str, float, float, float]:
                 done = counter.fetch_inc() + 1
                 print(f"{done}/{ds.to_process_rows_count}: {row_id}")
+                # if done == 4:
+                #     raise Exception()
                 return (
                     row_id,
                     int(row_id) * 1.0,
@@ -249,4 +226,5 @@ if __name__ == "__main__":
                 )
 
             ds.fill(generate_value)
-        dataset_manager.remove_failures_in_place({"2"})
+        # dataset_manager.remove_failures_in_place({"2"})
+        path.unlink(missing_ok=True)
